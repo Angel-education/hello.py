be60c1facfcd9203144a912e36da0089d2ab1ff4
Angel-education <s3852211@students.fscj.edu>
2025-11-20 03:04:22 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@
 # 11/20/25
 # A simple Python program to print a message
 
+# Angel Cosino
+# 11/20/25
+# A simple Python program to print a message
+
+print("Hello, world!")
+
 print("Hello, world!")
